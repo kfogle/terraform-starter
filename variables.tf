dfe4ae98b836e4ed7da04e27697f5b210d4a75ab
kfogle <25804216+kfogle@users.noreply.github.com>
2025-05-01 22:06:29 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "github_app_namespace" {
   description = "The namespace of the GitHub App to use for this stack"
   type        = string
-  default     = "Spacelift (kfogle-cloverlytech)"
+  default     = null
 }
